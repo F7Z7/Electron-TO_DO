aa1d78b070f28824232122b4a12da47cd4379e90
farza <farzanrichu610@gmai.com>
2025-03-30 01:41:13 +0530

    started working on script

--- src/renderer.js
+++ src/renderer.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const task = document.getElementById('inputTask');
-    const taskCounter = document.getElementById('taskCounter');
-    const listItems = document.getElementsByClassName('listItems');
+    console.log("hey script started");
 
-    function add() {
+    const taskInput = document.getElementById('inputTask');
+    const taskCounter = document.getElementById('counter');
+    const listItems = document.querySelector('.listItems');
+    const procrastinateBtn = document.getElementById('proCast');
+    const deleteBtn = document.getElementById('delete');
 
-        if (task.value.trim() == '') {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || []; // Load tasks from localStorage
+
+    function addTask() {
+        const taskText = taskInput.value.trim();
+        if (taskText === '') {
             alert('Please enter a task!');
+            return;
         }
-
     }
 })
